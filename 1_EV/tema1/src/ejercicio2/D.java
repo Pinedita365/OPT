23dcdb318ec10c68081cc0afe128287ab9a06d8a
Pinedita365 <javiercoco2201@gmail.com>
2025-09-24 08:57:36 +0200

    D

--- 1_EV/tema1/src/ejercicio2/D.java
+++ 1_EV/tema1/src/ejercicio2/D.java
@@ -1,5 +1,34 @@
 package ejercicio2;
 
+import java.util.Scanner;
+
 public class D {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] array = new int[10];
+        System.out.println("Digame 5 números de manera ordenada (crecientemente)");
+        for (int i = 0; i < 5; i++) {
+            array[i] = s.nextInt();
+        }
+        System.out.println("Lista: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println((i + 1) + ". " + array[i]);
+        }
+        int cont = 5;
+        do { 
+            System.out.println("Digame un número más y lo pondre dentro de la lista: ");
+            array[cont] = s.nextInt();
+            System.out.println("Lista actual: ");
+            for (int i = 0; i < array.length; i++) {
+                System.out.println((i + 1) + ". " + array[i]);
+            }
+            cont++;
+        } while (cont < 10);
+        funciones.MetodoBurbuja(array);
+        System.out.println("Lista compleatada.");
+        for (int i = 0; i < array.length; i++) {
+                System.out.println((i + 1) + ". " + array[i]);
+            }
+
+    }
 }
--- 1_EV/tema1/src/ejercicio2/funciones.java
+++ 1_EV/tema1/src/ejercicio2/funciones.java
@@ -5,5 +5,18 @@ public class funciones {
         int n = (int)(Math.random()*11);
         return n;
     }
-    
+    public static int[] MetodoBurbuja(int[] array){
+        int aux;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    aux = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = aux;
+                }
+                
+            }
+        }
+        return array;
+    }
 }
